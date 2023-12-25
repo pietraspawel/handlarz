9d5d6f2f8be3a31b4d4dc29198d36992ce8f45cf
pietraspawel <pawel.z.pietras@gmail.com>
2023-12-25 09:52:13 +0100

    render map-draft

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -18,16 +18,58 @@ class GameController extends AbstractController
         $filepath = $projectDir . '/config/game/maps/' . $map . '.yaml';
 
         if ($map == 'random') {
+            $config = ['random'];
             $json = '{"random"}';
+            $style = [];
         } elseif (file_exists($filepath)) {
             $config = Yaml::parseFile($filepath);
             $json = json_encode($config);
+
+            $styleMapGridTemplateColumns = '';
+            for ($i = 0; $i < $config['world']['xSize']; $i++) {
+                $styleMapGridTemplateColumns .= 'auto ';
+            }
+            $styleMapGridTemplateColumns .= ';';
+
+            $backgroundColors = [];
+            for ($y = 1; $y <= $config['world']['ySize']; $y++) {
+                for ($x = 1; $x <= $config['world']['xSize']; $x++) {
+                    $backgroundColors[$x][$y] = $this->generateTileBackgroundColor();
+                }
+            }
+
+            $style = [
+                'map' => [
+                    'grid_template_columns' => $styleMapGridTemplateColumns,
+                    'background_colors' => $backgroundColors,
+                ],
+            ];
         } else {
             return $this->redirectToRoute('app_home');
         }
 
         return $this->render('game/index.html.twig', [
+            'config' => $config,
             'json' => $json,
+            'style' => $style,
         ]);
+    }
+
+    /**
+     * Generate random background color.
+     */
+    private function generateTileBackgroundColor(): string
+    {
+        $colour = mt_rand(0, 9); //0 red 2-8 green 9 blue
+        $green = mt_rand(225, 255);
+        $red = 0;
+        $blue = 0;
+        if ($colour == 0) {
+            $red = mt_rand(0, 150);
+        }
+        if ($colour == 9) {
+            $blue = mt_rand(0, 150);
+        }
+        return "$red, $green, $blue";
     }
 }
