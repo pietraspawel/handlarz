777ff128b2a79d65836259558951544b4f243bac
pietraspawel <pawel.z.pietras@gmail.com>
2023-12-25 13:31:10 +0100

    init World.goods

---
+++ public/assets/js/src/Good.js
@@ -0,0 +1,8 @@
+class Good
+{
+    price;
+    quantity;
+    constructor(name) {
+        this.name = name;
+    }
+}
--- public/assets/js/src/World.js
+++ public/assets/js/src/World.js
@@ -2,4 +2,10 @@ class World
 {
     constructor (data) {
         this.turnsLeft = data.world.turnsAmount;
+        this.goods = [];
+        for (let key in data.world.cities[0].goods) {
+            let goodName = data.world.cities[0].goods[key].name;
+            this.goods.push(new Good(goodName))
+        }
+    }
 }
