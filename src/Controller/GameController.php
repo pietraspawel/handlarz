30eddf9e03b1aeb4072159bc12d2af8178cfba5d
pietraspawel <pawel.z.pietras@gmail.com>
2023-12-25 08:33:54 +0100

    load map configuration file

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Yaml\Yaml;
 
 class GameController extends AbstractController
 {
@@ -13,8 +14,20 @@ class GameController extends AbstractController
      */
     public function index(string $map): Response
     {
+        $projectDir = $this->getParameter('kernel.project_dir');
+        $filepath = $projectDir . '/config/game/maps/' . $map . '.yaml';
+
+        if ($map == 'random') {
+            $json = '{"random"}';
+        } elseif (file_exists($filepath)) {
+            $config = Yaml::parseFile($filepath);
+            $json = json_encode($config);
+        } else {
+            return $this->redirectToRoute('app_home');
+        }
+
         return $this->render('game/index.html.twig', [
-            'name' => $map,
+            'json' => $json,
         ]);
     }
 }
