5bc1f527d6739cd2dbc4a78befdf2253f23f6b9d
Paweł Pietras <pawel.z.pietras@gmail.com>
2026-05-17 12:11:41 +0200

    ref main.js

--- public/js/game/main.js
+++ public/js/game/main.js
@@ -3,17 +3,40 @@ $().ready(() => {
     let world = new World(data);
     let player = new Trader(world);
     let aiPlayerStrategy = new GreedyStrategy();
-    const aiPlayers = [ 
-        new TraderAI(world, 0, 'AITrader0', world.cities[0], aiPlayerStrategy),
-        new TraderAI(world, 1, 'AITrader1', world.cities[1], aiPlayerStrategy),
-        new TraderAI(world, 2, 'AITrader2', world.cities[2], aiPlayerStrategy),
-        new TraderAI(world, 3, 'AITrader3', world.getRandomCity(), aiPlayerStrategy),
-        new TraderAI(world, 4, 'AITrader4', world.getRandomCity(), aiPlayerStrategy),
-        new TraderAI(world, 5, 'AITrader5', world.getRandomCity(), aiPlayerStrategy),
+    const aiPlayers = [
+        new TraderAI(world, 0, "AITrader0", world.cities[0], aiPlayerStrategy),
+        new TraderAI(world, 1, "AITrader1", world.cities[1], aiPlayerStrategy),
+        new TraderAI(world, 2, "AITrader2", world.cities[2], aiPlayerStrategy),
+        new TraderAI(
+            world,
+            3,
+            "AITrader3",
+            world.getRandomCity(),
+            aiPlayerStrategy,
+        ),
+        new TraderAI(
+            world,
+            4,
+            "AITrader4",
+            world.getRandomCity(),
+            aiPlayerStrategy,
+        ),
+        new TraderAI(
+            world,
+            5,
+            "AITrader5",
+            world.getRandomCity(),
+            aiPlayerStrategy,
+        ),
     ];
 
-    GameView.refreshAll(world, player, aiPlayers);
-
     let tooltipsView = new TooltipsView();
-    let gameEventsHandler = new GameEventsHandler(aiPlayers, player, world, tooltipsView);
+    let gameEventsHandler = new GameEventsHandler(
+        aiPlayers,
+        player,
+        world,
+        tooltipsView,
+    );
+
+    GameView.refreshAll(world, player, aiPlayers);
 });
