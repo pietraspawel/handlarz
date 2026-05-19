65f8bc232088bf77afd949ab20e2a0010f038930
Paweł Pietras <pawel.z.pietras@gmail.com>
2026-05-19 17:53:24 +0200

    ref TraderView, CityView

--- public/js/game/view/CityView.js
+++ public/js/game/view/CityView.js
@@ -42,4 +42,26 @@ class CityView {
 			}
 		}
 	}
+
+	static refreshSellButtons(trader) {
+		let container = $(".city-info .sell");
+		container.find("td").remove();
+		for (let i in trader.goods) {
+			let string = Library.separateThousands(trader.goods[i].quantity);
+			let element = `<td><button class="btn btn-info" data-id="${i}">${string}</button></td>`;
+			container.append(element);
+		}
+	}
+
+	static refreshBuyButtons(trader) {
+		let container = $(".city-info .buy");
+		container.find("td").remove();
+		for (let i in trader.goods) {
+			let string = Library.separateThousands(
+				Math.floor(trader.gold / trader.city.goods[i].price),
+			);
+			let element = `<td><button class="btn btn-info" data-id="${i}">${string}</button></td>`;
+			container.append(element);
+		}
+	}
 }
--- public/js/game/view/TraderView.js
+++ public/js/game/view/TraderView.js
@@ -4,36 +4,14 @@ class TraderView {
 		TraderView.refreshCargo(trader);
 		if (trader.isInCity()) {
 			TraderView.refreshWealth(trader);
-			TraderView.refreshBuyButtons(trader);
-			TraderView.refreshSellButtons(trader);
+			CityView.refreshBuyButtons(trader);
+			CityView.refreshSellButtons(trader);
 		}
 	}
 
 	static refreshElementsAfterTrade(trader) {
 		TraderView.refreshCargo(trader);
 		TraderView.refreshWealth(trader);
-	}
-
-	static refreshSellButtons(trader) {
-		let container = $(".city-info .sell");
-		container.find("td").remove();
-		for (let i in trader.goods) {
-			let string = Library.separateThousands(trader.goods[i].quantity);
-			let element = `<td><button class="btn btn-info" data-id="${i}">${string}</button></td>`;
-			container.append(element);
-		}
-	}
-
-	static refreshBuyButtons(trader) {
-		let container = $(".city-info .buy");
-		container.find("td").remove();
-		for (let i in trader.goods) {
-			let string = Library.separateThousands(
-				Math.floor(trader.gold / trader.city.goods[i].price),
-			);
-			let element = `<td><button class="btn btn-info" data-id="${i}">${string}</button></td>`;
-			container.append(element);
-		}
 	}
 
 	static refreshCargo(trader) {
