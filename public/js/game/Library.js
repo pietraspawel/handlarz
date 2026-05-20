7cb4121e7384a659ed2e7dccab07cddb548dcd32
Paweł Pietras <pawel.z.pietras@gmail.com>
2026-05-20 07:33:13 +0200

    ref Library

--- public/js/game/Library.js
+++ public/js/game/Library.js
@@ -1,5 +1,4 @@
-class Library
-{
+class Library {
     /**
      * For 1234567890 returns 1 234 567 890.
      */
@@ -9,10 +8,10 @@ class Library
         let rest = integerNumber.toString();
         while (rest.length > 3) {
             subn.push(" " + rest.slice(-3));
-            rest = rest.substr(0, rest.length-3);
+            rest = rest.substr(0, rest.length - 3);
         }
         ret = rest;
-        for (var i = subn.length-1; i >= 0; i--) {
+        for (var i = subn.length - 1; i >= 0; i--) {
             ret += subn[i];
         }
         return ret;
