70cd3419ca8e2050ff81e929f57f539d59d60abe
Paweł Pietras <pawel.z.pietras@gmail.com>
2026-05-22 17:56:50 +0200

    up Snapshot

--- public/js/game/log/Snapshot.js
+++ public/js/game/log/Snapshot.js
@@ -1,10 +1,23 @@
 class Snapshot {
     name;
+    position;
+    cityName;
+    goods;
+    gold;
 
-    constructor({trader}) {
-        this.name = 'Player';
+    constructor({ trader }) {
+        this.name = "Player";
         if (trader instanceof TraderAI) {
             this.name = trader.name;
         }
+        this.position = { ...trader.position };
+        this.cityName = trader.city?.name ?? "";
+        this.goods = [];
+        for (const good of trader.goods) {
+            const obj = new Good(good.name);
+            obj.quantity = good.quantity;
+            this.goods.push(obj);
+        }
+        this.gold = trader.gold;
     }
 }
