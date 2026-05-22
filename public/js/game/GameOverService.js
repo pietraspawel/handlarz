fac5f49ab60a55251d460ef3b0cd4acc49fdf981
Paweł Pietras <pawel.z.pietras@gmail.com>
2026-05-22 05:05:51 +0200

    ref GameOverService

--- public/js/game/GameOverService.js
+++ public/js/game/GameOverService.js
@@ -1,18 +1,18 @@
 class GameOverService {
-	static async gameOver(world, player, aiPlayersArray) {
-		await fetch("/game-over/save", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				map: world.map,
-				score: player.gold,
-				highscore: world.highscore,
-				aiPlayers: aiPlayersArray,
-			}),
-		});
+    static async gameOver(world, player, aiPlayersArray) {
+        await fetch("/game-over/save", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                map: world.map,
+                score: player.gold,
+                highscore: world.highscore,
+                aiPlayers: aiPlayersArray,
+            }),
+        });
 
-		location.replace("/game-over");
-	}
+        location.replace("/game-over");
+    }
 }
