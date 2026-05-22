d68f3bc6424fe9e10e630de3d479c63f6bce6bdc
Paweł Pietras <pawel.z.pietras@gmail.com>
2026-05-22 17:19:45 +0200

    up TurnLog

--- public/js/game/log/TurnLog.js
+++ public/js/game/log/TurnLog.js
@@ -1,13 +1,28 @@
+// Przechowuje logi jednej tury.
 class TurnLog {
     number;
-    traderLog;
+    tradersLogs;
 
-    constructor(number) {
-        this.number = number;
-        this.traderLog = [];
+    constructor({ turnNumber, trader, aiTraders }) {
+        this.number = turnNumber;
+        this.tradersLogs = [];
+        this.addTraderLogs({ trader });
+        for (const aiTrader of aiTraders) {
+            this.addTraderLogs({ trader: aiTrader });
+        }
     }
 
-    addTraderLog(traderLog) {
-        this.traderLog.push(traderLog);
+    addTraderLogs({ trader }) {
+        const traderLogs = new TraderLogs({ trader });
+        this.tradersLogs.push(traderLogs);
+    }
+
+    addSnapshot({ trader }) {
+        const traderLogs = this.findTradersLogsByTrader(trader);
+        traderLogs.addSnapshot({ trader });
+    }
+
+    findTradersLogsByTrader(trader) {
+        return this.tradersLogs.find((element) => element.trader === trader);
     }
 }
