3a5112eab62e8720f64bbb3281ed7f2d4ecca28c
Paweł Pietras <pawel.z.pietras@gmail.com>
2026-05-22 17:19:03 +0200

    add TraderLogs

---
+++ public/js/game/log/TraderLogs.js
@@ -0,0 +1,15 @@
+// Przechowuje logi jednego Tradera.
+class TraderLogs {
+	trader;
+	log;
+
+	constructor({ trader }) {
+		this.trader = trader;
+		this.log = [];
+	}
+
+	addSnapshot({ trader }) {
+		const log = new Snapshot({ trader });
+		this.log.push(log);
+	}
+}
