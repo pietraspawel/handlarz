11f5158387ad8d8aa26a32e197eff8dd722a4b36
Paweł Pietras <pawel.z.pietras@gmail.com>
2026-05-23 05:13:15 +0200

    ref TurnSystem

--- public/js/game/TurnSystem.js
+++ public/js/game/TurnSystem.js
@@ -12,60 +12,61 @@
 // Jeśli Gracz jest u celu to sprzedaj wszystko, kup towar, wyznacz nowy cel.
 
 class TurnSystem {
-	static autoTurnTimeout = null;
+    static autoTurnTimeout = null;
 
-	static nextTurn(gameContext, trader, aiTraders) {
-		let world = gameContext.world;
+    static nextTurn(gameContext, trader, aiTraders) {
+        let world = gameContext.world;
 
-		for (const aiTrader of aiTraders) {
-			aiTrader.turn(world);
-		}
-		trader.turn(world);
-		world.turnsLeft--;
-		GameView.refreshElementsAfterTurn(world, trader, aiTraders);
+        for (const aiTrader of aiTraders) {
+            aiTrader.turn(world);
+        }
+        trader.turn(world);
+        world.turnsLeft--;
+        GameView.refreshElementsAfterTurn(world, trader, aiTraders);
 
-		if (world.turnsLeft <= 0) {
-			GameOverService.gameOver(gameContext, trader, aiTraders);
-		}
+        if (world.turnsLeft <= 0) {
+            GameOverService.gameOver(gameContext, trader, aiTraders);
+        }
 
-		// gameContext.gameLog.startTurn({ trader, aiTraders });
-	}
+        gameContext.gameLog.startTurn({ trader, aiTraders });
+        console.log(gameContext.gameLog);
+    }
 
-	static autoTurns(gameContext, trader, aiTraders) {
-		let world = gameContext.world;
-		if (this.autoTurnTimeout) {
-			return;
-		}
+    static autoTurns(gameContext, trader, aiTraders) {
+        let world = gameContext.world;
+        if (this.autoTurnTimeout) {
+            return;
+        }
 
-		const loop = () => {
-			if (trader.isInCity() || world.turnsLeft <= 0) {
-				TurnSystem.stopAutoTurns();
-				return;
-			}
+        const loop = () => {
+            if (trader.isInCity() || world.turnsLeft <= 0) {
+                TurnSystem.stopAutoTurns();
+                return;
+            }
 
-			TurnSystem.nextTurn(gameContext, trader, aiTraders);
-			this.autoTurnTimeout = setTimeout(loop, 200);
-		};
+            TurnSystem.nextTurn(gameContext, trader, aiTraders);
+            this.autoTurnTimeout = setTimeout(loop, 200);
+        };
 
-		this.autoTurnTimeout = setTimeout(loop, 200);
-	}
+        this.autoTurnTimeout = setTimeout(loop, 200);
+    }
 
-	static stopAutoTurns() {
-		if (this.autoTurnTimeout) {
-			clearTimeout(this.autoTurnTimeout);
-			this.autoTurnTimeout = null;
-		}
-	}
+    static stopAutoTurns() {
+        if (this.autoTurnTimeout) {
+            clearTimeout(this.autoTurnTimeout);
+            this.autoTurnTimeout = null;
+        }
+    }
 
-	static fastForwardAutoTurns(gameContext, trader, aiTraders) {
-		let world = gameContext.world;
-		TurnSystem.stopAutoTurns();
+    static fastForwardAutoTurns(gameContext, trader, aiTraders) {
+        let world = gameContext.world;
+        TurnSystem.stopAutoTurns();
 
-		while (trader.isTravelling() && world.turnsLeft > 0) {
-			TurnSystem.nextTurn(gameContext, trader, aiTraders);
-			if (world.turnsLeft <= 0) {
-				break;
-			}
-		}
-	}
+        while (trader.isTravelling() && world.turnsLeft > 0) {
+            TurnSystem.nextTurn(gameContext, trader, aiTraders);
+            if (world.turnsLeft <= 0) {
+                break;
+            }
+        }
+    }
 }
