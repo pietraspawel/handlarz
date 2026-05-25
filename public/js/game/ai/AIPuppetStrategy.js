e74b731ea88d1d88fbf75cfe095cc1406b0c503b
Paweł Pietras <pawel.z.pietras@gmail.com>
2026-05-25 09:43:51 +0200

    aa AIPuppetStartegy

---
+++ public/js/game/ai/AIPuppetStrategy.js
@@ -0,0 +1,39 @@
+class AIPuppetStrategy extends AIStrategy {
+    commands;
+    infiniteLoop;
+    commandPointer;
+    loopPointer;
+
+    constructor(script) {
+        super();
+        this.commands = script.commands ?? [];
+        this.infiniteLoop = script.infiniteLoop ?? [];
+        this.commandPointer = 0;
+        this.loopPointer = 0;
+    }
+
+    decide(world, trader) {
+        let command = null;
+
+        if (this.commandPointer < this.commands.length) {
+            command = this.commands[this.commandPointer];
+            this.commandPointer++;
+        } else if (this.infiniteLoop.length > 0) {
+            command =
+                this.infiniteLoop[this.loopPointer % this.infiniteLoop.length];
+            this.loopPointer++;
+        }
+
+        if (!command) {
+            return {
+                city: null,
+                goodId: null,
+            };
+        }
+
+        return {
+            city: world.findCityByName(command.city),
+            goodId: world.findGoodIdByName(command.good),
+        };
+    }
+}
