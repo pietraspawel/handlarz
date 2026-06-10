d79a6d5a44ff0646f1aba8cab492c1286139ebe1
Paweł Pietras <pawel.z.pietras@gmail.com>
2026-06-10 15:09:27 +0200

    tab to spaces

--- public/js/game/GameContext.js
+++ public/js/game/GameContext.js
@@ -1,16 +1,16 @@
 class GameContext {
-	static GAME_MODE = { MANUAL: "manual", AUTO_TURNS: "autoTurns" };
-	static MAX_NON_HIDDEN_AI = 3;
+    static GAME_MODE = { MANUAL: "manual", AUTO_TURNS: "autoTurns" };
+    static MAX_NON_HIDDEN_AI = 3;
 
-	gameMode;
-	gameLog;
-	world;
-	highscore;
+    gameMode;
+    gameLog;
+    world;
+    highscore;
 
-	constructor({ gameMode, data, world }) {
-		this.gameMode = gameMode;
-		this.world = world;
-		this.gameLog = new GameLog(world.map);
-		this.highscore = parseInt(data.game.highscore);
-	}
+    constructor({ gameMode, data, world }) {
+        this.gameMode = gameMode;
+        this.world = world;
+        this.gameLog = new GameLog(world.map);
+        this.highscore = parseInt(data.game.highscore);
+    }
 }
--- public/js/game/view/GameView.js
+++ public/js/game/view/GameView.js
@@ -1,36 +1,36 @@
 class GameView {
-	static refreshAll(gameContext, trader, aiTraders) {
-		const world = gameContext.world;
-		GameView.refreshMapRecordView(gameContext.highscore);
-		CityView.refreshCityPrices(world.cities);
-		this.refreshElementsAfterTurn(world, trader, aiTraders);
-		this.refreshElementsAfterTrade(trader);
-	}
+    static refreshAll(gameContext, trader, aiTraders) {
+        const world = gameContext.world;
+        GameView.refreshMapRecordView(gameContext.highscore);
+        CityView.refreshCityPrices(world.cities);
+        this.refreshElementsAfterTurn(world, trader, aiTraders);
+        this.refreshElementsAfterTrade(trader);
+    }
 
-	static refreshElementsAfterTurn(world, trader, aiTraders) {
-		GameView.refreshTurnsLeftView(world.turnsLeft);
-		TraderView.refreshElementsAfterTurn(trader);
-		TraderAIView.refreshElementsAfterTurn(aiTraders);
-		if (trader.isInCity()) {
-			WorldView.setTravelMode(false);
-			CityView.setTravelMode(false, trader);
-		} else {
-			WorldView.setTravelMode(true);
-			CityView.setTravelMode(true, trader);
-		}
-	}
+    static refreshElementsAfterTurn(world, trader, aiTraders) {
+        GameView.refreshTurnsLeftView(world.turnsLeft);
+        TraderView.refreshElementsAfterTurn(trader);
+        TraderAIView.refreshElementsAfterTurn(aiTraders);
+        if (trader.isInCity()) {
+            WorldView.setTravelMode(false);
+            CityView.setTravelMode(false, trader);
+        } else {
+            WorldView.setTravelMode(true);
+            CityView.setTravelMode(true, trader);
+        }
+    }
 
-	static refreshElementsAfterTrade(trader) {
-		CityView.refreshElementsAfterTrade(trader);
-		TraderView.refreshElementsAfterTrade(trader);
-	}
+    static refreshElementsAfterTrade(trader) {
+        CityView.refreshElementsAfterTrade(trader);
+        TraderView.refreshElementsAfterTrade(trader);
+    }
 
-	static refreshTurnsLeftView(turnsLeft) {
-		$(".player-info .turns-left").text(turnsLeft);
-	}
+    static refreshTurnsLeftView(turnsLeft) {
+        $(".player-info .turns-left").text(turnsLeft);
+    }
 
-	static refreshMapRecordView(highscore) {
-		let string = MathLibrary.formatNumber(highscore) + " $";
-		$(".player-info .map-record").text(string);
-	}
+    static refreshMapRecordView(highscore) {
+        let string = MathLibrary.formatNumber(highscore) + " $";
+        $(".player-info .map-record").text(string);
+    }
 }
