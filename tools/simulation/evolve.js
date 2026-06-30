c2d2287a0c11bc397d06e5e6c661ed0eddafb709
Paweł Pietras <pawel.z.pietras@gmail.com>
2026-06-30 07:37:43 +0200

    train -> evolve

--- tools/simulation/evolve.js
+++ tools/simulation/evolve.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 // CLI
 const mapName = process.argv[2];
 if (!mapName) {
-    console.error("❌ Podaj nazwę mapy: node train.js astana");
+    console.error("❌ Podaj nazwę mapy: node evolve.js astana");
     process.exit(1);
 }
 
